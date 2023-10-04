fa859fd599285adb1fb807fe6d32f2f7e611c11d
Javier Middleton <javier.middleton13@gmail.com>
2023-10-04 20:48:34 -0300

    second commit- install of React Bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <>
-      <div></div>
+      <div>
+        <BootstrapForm />
+      </div>
     </>
   );
 }
